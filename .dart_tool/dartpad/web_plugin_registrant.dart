bfec1bea76de41472a7155214e6e2464dc73b101
MakuDevT <markangelo.u.unida@gmail.com>
2023-06-02 13:24:52 +0800

    added firebase auth and firebase ui dependency

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,11 +6,15 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:desktop_webview_auth/desktop_webview_auth_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  DesktopWebviewAuthWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
