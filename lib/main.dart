5400f0f5c3e50f928ab91d068ee408b072757cee
MakuDevT <markangelo.u.unida@gmail.com>
2023-06-02 12:52:41 +0800

    setup needed files for firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
