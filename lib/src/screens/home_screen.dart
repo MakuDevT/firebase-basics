1db663ed1a33b6bac8e0454d05820887cf2f539e
MakuDevT <markangelo.u.unida@gmail.com>
2023-06-06 23:05:18 +0800

    addded faker dependency

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:faker/faker.dart';
+import 'package:firebase_basics/src/data/firestore_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -19,7 +21,13 @@ class HomeScreen extends ConsumerWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          debugPrint('Not implemented');
+          final user = ref.read(firebaseAuthProvider).currentUser;
+          final faker = Faker();
+          final title = faker.job.title();
+          final company = faker.company.name();
+          ref
+              .read(firestoreRepositoryProvider)
+              .addJob(user!.uid, title, company);
         },
       ),
     );
