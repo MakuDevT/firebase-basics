9e46dea0ed41b84342cc9dc38962af4a41c8a54c
MakuDevT <markangelo.u.unida@gmail.com>
2023-06-07 21:46:27 +0800

    added update and delete functionality

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -1,21 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_basics/src/data/job.dart';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FirestoreRepository {
   FirestoreRepository(this._firestore);
   final FirebaseFirestore _firestore;
 
-  Future<void> addJob(String uid, String title, String company) async {
-    final docRef = await _firestore
-        .collection('jobs')
-        .add({'uid': uid, 'title': title, 'company': company});
-    debugPrint(docRef.id);
-  }
+  Future<void> addJob(String uid, String title, String company) =>
+      (_firestore.collection('jobs').add({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      }));
+  Future<void> updateJob(
+          String uid, String jobId, String title, String company) =>
+      (_firestore.doc('jobs/$jobId').update({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      }));
+  Future<void> deleteJob(String uid, String jobId) =>
+      _firestore.doc('jobs/$jobId').delete();
 
-  Query<Map<String, dynamic>> jobsQuery() {
-    return _firestore.collection('jobs');
+  Query<Job> jobsQuery() {
+    //converter let us plug in our data serialization logic
+    //so we don't have to deal with key value pairs in the rest of the codebase
+    return _firestore.collection('jobs').withConverter(
+        fromFirestore: (snapshot, _) => Job.fromMap(snapshot.data()!),
+        toFirestore: (job, _) => job.toMap());
   }
 }
 
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:faker/faker.dart';
+import 'package:faker/faker.dart' hide Job;
 import 'package:firebase_basics/src/data/firestore_repository.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../data/job.dart';
 import '../routing/app_router.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -43,13 +44,31 @@ class JobListView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final firestoreRepository = ref.watch(firestoreRepositoryProvider);
-    return FirestoreListView(
+    return FirestoreListView<Job>(
       query: firestoreRepository.jobsQuery(),
-      itemBuilder: (BuildContext context,
-          QueryDocumentSnapshot<Map<String, dynamic>> doc) {
-        return ListTile(
-          title: Text(doc['title']),
-          subtitle: Text(doc['company']),
+      itemBuilder: (BuildContext context, QueryDocumentSnapshot<Job> doc) {
+        final job = doc.data();
+        return Dismissible(
+          key: Key(doc.id),
+          background: const ColoredBox(color: Colors.red),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            final user = ref.read(firebaseAuthProvider).currentUser;
+            ref.read(firestoreRepositoryProvider).deleteJob(user!.uid, doc.id);
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
+              final title = faker.job.title();
+              final company = faker.company.name();
+              ref
+                  .read(firestoreRepositoryProvider)
+                  .updateJob(user!.uid, doc.id, title, company);
+            },
+          ),
         );
       },
     );
